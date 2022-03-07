ddd06627c4984fa00e0214ce41e17fed48d08589
tuanlahe151276 <tuanlahe151276@fpt.edu.vn>
2022-03-07 22:39:35 +0700

    Load list product

--- src/java/Dao/DAO.java
+++ src/java/Dao/DAO.java
@@ -4,11 +4,42 @@
  * and open the template in the editor.
  */
 package Dao;
+import Context.DBContext;
+import Entity.Product;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author anhtu
  */
 public class DAO {
+    Connection conn = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
     
+public List<Product> getAllProduct(){
+    List<Product> list = new ArrayList<>();
+    String query = "select * from product";
+    try {
+        conn = new DBContext().getConnection();
+        ps= conn.prepareStatement(query);
+        rs = ps.executeQuery();
+        while(rs.next()){
+        list.add(new Product(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getDouble(4), rs.getString(5), rs.getString(6)));
+        }
+    } catch (Exception e) {
+    }
+    return list;
+}
+    public static void main(String[] args) {
+        DAO dao = new DAO();
+        List<Product> list = dao.getAllProduct();
+        for (Product product : list) {
+            System.out.println(product);
+        }
+    }
 }
--- src/java/Entity/Product.java
+++ src/java/Entity/Product.java
@@ -10,5 +10,79 @@ package Entity;
  * @author anhtu
  */
 public class Product {
+    private int ID;
+    private String name;
+    private String image;
+    private double price;
+    private String title;
+    private String description;
+
+    public Product() {
+    }
+
+    public Product(int ID, String name, String image, double price, String title, String description) {
+        this.ID = ID;
+        this.name = name;
+        this.image = image;
+        this.price = price;
+        this.title = title;
+        this.description = description;
+    }
+
+    
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" + "ID=" + ID + ", name=" + name + ", image=" + image + ", price=" + price + ", title=" + title + ", description=" + description + '}';
+    }
+
     
 }
+
