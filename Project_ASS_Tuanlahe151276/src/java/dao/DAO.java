0dd979f3f127eb2f4b77a23a011d77400e9a8a5f
tuanlahe151276 <tuanlahe151276@fpt.edu.vn>
2022-03-15 22:23:57 +0700

    Signup control

--- Project_ASS_Tuanlahe151276/src/java/dao/DAO.java
+++ Project_ASS_Tuanlahe151276/src/java/dao/DAO.java
@@ -171,6 +171,43 @@ public class DAO {
         
         return null;
     }
+    public Account CheckExist(String user){
+    
+        String query ="select* from Account\n" +
+"where [USER] = ? "
+              ;
+        try {
+            conn = new DBContext().getConnection();//mo ket noi voi sql
+            ps = conn.prepareStatement(query);
+            ps.setString(1, user);
+            rs = ps.executeQuery(); 
+            while(rs.next()){
+            return new Account(rs.getInt(1),
+                    rs.getString(2),
+                    rs.getString(3), 
+                    rs.getInt(4), 
+                    rs.getInt(5));
+            }
+            
+        } catch (Exception e) {
+        }
+        
+        return null;
+    }
+    public  void signup(String user, String pass){
+    String query = "insert into Account\n" +
+"values(?,?,0,0)";
+        try {
+            conn = new DBContext().getConnection();//mo ket noi voi sql
+            ps = conn.prepareStatement(query);
+            ps.setString(1, user);
+            ps.setString(2, pass);
+            ps.executeUpdate();
+           
+        } catch (Exception e) {
+        }
+    
+    }
     public static void main(String[] args) {
         DAO dao = new DAO();
        
--- Project_ASS_Tuanlahe151276/web/Login.jsp
+++ Project_ASS_Tuanlahe151276/web/Login.jsp
@@ -37,7 +37,7 @@
                 <h1 class="h3 mb-3 font-weight-normal" style="text-align: center"> Sign up</h1>
                 <input name="user" type="text" id="user-name" class="form-control" placeholder="User name" required="" autofocus="">
                 <input name="pass" type="password" id="user-pass" class="form-control" placeholder="Password" required autofocus="">
-                <input name="repass" type="password" id="user-repeatpass" class="form-control" placeholder="Repeat Password" required autofocus="">
+                <input name="repass" type="password" id="user-repeatpass" class="form-control" placeholder="Confirm Password" required autofocus="">
 
                 <button class="btn btn-primary btn-block" type="submit"><i class="fas fa-user-plus"></i> Sign Up</button>
                 <a href="#" id="cancel_signup"><i class="fas fa-angle-left"></i> Back</a>
